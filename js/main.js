6cfd4ed5704d29ad2c736688d18460c0cf5f7bc4
nataries <natalizastavnaya@icloud.com>
2026-04-14 09:53:34 +0300

    Added push screenshot

--- js/main.js
+++ js/main.js
@@ -1,7 +1,95 @@
-import {E, cube, square } from "./math.js";
-console.log("Значение Е: ", E);
-console.log(square(13));
-console.log(cube(3));
+function checkInventory() {
+  return new Promise((resolve, reject) => {
+    const inStock = true;
+    if (inStock) {
+      resolve({
+        name: "Macbook",
+        price: 1000,
+      });
+    } else {
+      reject("Товара нет в наличии");
+    }
+  });
+}
+
+checkInventory(false)
+  .then((inStock) => console.log("Товар есть"))
+  .catch((error) => console.log("Ошибка"));
+
+// console.log("Промисы");
+// const simplePromise = new Promise((resolve, reject) => {
+//     const success = true;
+//     if (success) {
+//         resolve("Операция выполнена успешно!");
+//     } else {
+//         reject("Произошла ошибка!");
+//     }
+// });
+
+// simplePromise
+// .then((result) => console.log("Результат:", result))
+// .catch((error) => console.log("Ошибка: ", error));
+
+// function delay(ms) {
+//     return new Promise((resolve) => {
+//         setTimeout(() => {
+//             resolve(`Прошло ${ms} миллисекунд`);
+//         }, ms);
+//     });
+// }
+
+// delay(1000)
+// .then((message) => console.log(message));
+
+// function fetchUserData(userId) {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             if (userId > 0) {
+//                 resolve({
+//                     id: userId,
+//                     name: "Иван Иванов",
+//                     email: "ivan@example.com",
+//                 });
+//             } else {
+//                 reject("Неверный ID пользователя");
+//             }
+//         }, 1500);
+//     });
+// }
+
+// fetchUserData(1)
+// .then((user) => console.log("Пользователь: ", user))
+// .catch((error) => console.log("Ошибка", error));
+
+// function step1() {
+//     return new Promise((resolve) => {
+//         setTimeout(() => resolve("Шаг 1 завершен"), 500);
+//     });
+// }
+
+// function step2(previousResult) {
+//     return new Promise((resolve) => {
+//         setTimeout(() => resolve(`${previousResult} -> Шаг 2 завершён`), 500);
+//     });
+// }
+
+// function step3(previousResult) {
+//     return new Promise((resolve) => {
+//         setTimeout(() => resolve(`${previousResult} -> Шаг 3 завершён`), 500);
+//     });
+// }
+
+// step1()
+// .then((result1) => step2(result1))
+// .then((result2) => step3(result2))
+// .then((finalResult) => console.log("Финальный результат:", finalResult))
+// .catch((error) => console.log("Ошибка в цепочке", error));
+
+// Task 8.5
+//import {E, cube, square } from "./math.js";
+// console.log("Значение Е: ", E);
+// console.log(square(13));
+// console.log(cube(3));
 
 // import { greet, add, PI } from "./utils.js";
 // console.log("Модули");
@@ -155,4 +243,3 @@ console.log(cube(3));
 
 // const fullInfo = {...country, ...footballer};
 // console.log("Полная информация", fullInfo);
-
