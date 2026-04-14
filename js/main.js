852e13cb3bf0a0a887f572cd42ecac8ee44350b4
nataries <natalizastavnaya@icloud.com>
2026-04-14 09:09:08 +0300

    spread/rest in JS

--- js/main.js
+++ js/main.js
@@ -49,21 +49,91 @@
 // console.log();
 // printUser(user);
 
-const product = {
-    name: "iPhone",
-    price: "60000",
-    category: "15 Plus",
-    inStock: true,
-};
+// const product = {
+//     name: "iPhone",
+//     price: "60000",
+//     category: "15 Plus",
+//     inStock: true,
+// };
 
-const { name, price, category, inStock} = product;
-console.log(name, price, category, inStock);
+// const { name, price, category, inStock} = product;
+// console.log(name, price, category, inStock);
 
-function printProduct( {name, price, category, inStock}) {
-    console.log(`Название: ${name}`);
-    console.log(`Цена: ${price}`);
-    console.log(`Категория: ${category}`);
-    console.log(`В наличии: ${inStock}`);
+// function printProduct( {name, price, category, inStock}) {
+//     console.log(`Название: ${name}`);
+//     console.log(`Цена: ${price}`);
+//     console.log(`Категория: ${category}`);
+//     console.log(`В наличии: ${inStock}`);
+// }
+
+// printProduct(product);
+
+// console.log("Spread для массивов");
+// const arr1 = [1, 2, 3];
+// const arr2 = [4, 5, 6];
+// const combined = [...arr1, ...arr2];
+// console.log("Объединённый массив", combined);
+
+// const copy = [...arr1];
+// console.log("Копия массива: ", copy);
+
+// const extended = [0, ...arr1, 7, 8];
+// console.log("Расширенный массив: ", extended);
+
+// console.log("Spread для объектов");
+// const person = {
+//     name: "Иван",
+//     age: 30,
+// };
+
+// const address = {
+//     city: "Санкт-Петербург",
+//     street: "Невский проспект",
+// };
+
+// const fullInfo = {...person, ...address};
+// console.log("Полная информация: ", fullInfo);
+
+// const personCopy = {...person};
+// console.log("Копия объекта", personCopy);
+
+// const updated = {...person, age: 31, occupation: "Developer"};
+// console.log("Обновлённый объект", updated);
+
+// console.log("Rest оператор");
+// function sum(...numbers) {
+//     return numbers.reduce((total, num) => total + num, 0);
+// }
+// console.log("Сумма 1, 2, 3:", sum(1, 2, 3));
+// console.log("Сумма 1, 2, 3, 4, 5: ", sum(1, 2, 3, 4, 5));
+
+// const numbers = [10, 20, 30, 40, 50];
+// const [first, second, ...rest] = numbers;
+// console.log("Первое число", first);
+// console.log("Второе число", second);
+// console.log("Остальные числа", rest);
+
+//Task 7.5
+const nums1 = [77, 88, 99];
+const nums2 = [1, 3, 5];
+const combin = [...nums1, ...nums2];
+console.log("Объединение массивов ", combin);
+
+function findMax(...nums) {
+    return Math.max(...nums);
 }
 
-printProduct(product);
+console.log(findMax(88, 2, 87));
+
+const country = {
+    name: "Португалия",
+    capital: "Лиссабон"
+};
+
+const footballer = {
+    name: "Cristiano Ronaldo",
+    years: 41,
+};
+
+const fullInfo = {...country, ...footballer};
+console.log("Полная информация", fullInfo);
